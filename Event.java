aa8bf678efcff6b763fb2732bb895b1b9da49f95
Shilpa <shilpa.sinha3107@gmail.com>
2024-01-27 21:54:58 +0530

    Added getter and setter methods

--- Event.java
+++ Event.java
@@ -4,5 +4,36 @@ public class Event {
     private String colorScheme;
     private String backgroundImage;
 
-    
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getColorScheme() {
+        return colorScheme;
+    }
+
+    public String getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setColorScheme(String colorScheme) {
+        this.colorScheme = colorScheme;
+    }
+
+    public void setBackgroundImage(String backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
 }

55abdc96a0231181156c70e7ebbb000541333338
Shilpa <shilpa.sinha3107@gmail.com>
2024-01-27 21:59:25 +0530

    Added code for managing countdown events

--- EventService.java
+++ EventService.java
@@ -1,3 +1,16 @@
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class EventService {
-    
+    private List<Event> events = new ArrayList<>();
+
+    public List<Event> getAllEvents() {
+        return events;
+    }
+
+    public void addEvent(Event event) {
+        events.add(event);
+    }
 }

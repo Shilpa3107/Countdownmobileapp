dbfeb8f3c8fabc56040d23d2b35538df5fdf11ea
Shilpa <shilpa.sinha3107@gmail.com>
2024-01-30 23:51:40 +0530

    Define the function use by comments

--- Event.java
+++ Event.java
@@ -4,35 +4,39 @@ public class Event {
     private String colorScheme;    //for storing the name of the color
     private String backgroundImage;   //for storing the url of the background image
 
-    public String getName() {    
-        return name;
+    //getter methods
+
+    public String getName() {       //getting event name
+        return name;   
     }
 
-    public String getDate() {   
+    public String getDate() {        //getting event date
         return date;
     }
 
-    public String getColorScheme() {
+    public String getColorScheme() {      //getting color scheme
         return colorScheme;
     }
 
-    public String getBackgroundImage() {
+    public String getBackgroundImage() {       //getting backgroung image url
         return backgroundImage;
     }
 
-    public void setName(String name) {
+     //setter methods   
+
+    public void setName(String name) {         //storing event name to event variable
         this.name = name;
     }
 
-    public void setDate(String date) {
+    public void setDate(String date) {         //storing date name to date variable
         this.date = date;
     }
 
-    public void setColorScheme(String colorScheme) {
+    public void setColorScheme(String colorScheme) {        //storing color scheme to colorSCheme variable
         this.colorScheme = colorScheme;
     }
 
-    public void setBackgroundImage(String backgroundImage) {
+    public void setBackgroundImage(String backgroundImage) {     //storing background image url to baackgroundImage variable
         this.backgroundImage = backgroundImage;
     }
 }
